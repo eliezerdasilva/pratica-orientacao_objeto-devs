e50178c1c5ddbe2224fb7038f39f1ceb091b810d
Eliezer <frete7132@gmail.com>
2023-02-22 21:16:39 -0300

    types of methods

--- src/exemplo01/Carro.java
+++ src/exemplo01/Carro.java
@@ -1,5 +1,23 @@
 package exemplo01;
 
 public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	 
+	
 
+	void exibirCapacidade() {
+		System.out.println("capacidade de passageiros "+ numPassageiros);
+	}
+	double obterNumPassageiro() {
+		return numPassageiros -1;
+	}
+	double obterKm(double km) {
+		double soma = km * consumoCombustivel;
+		return soma;
+	}
 }
--- src/exemplo01/TesteCarro.java
+++ src/exemplo01/TesteCarro.java
@@ -3,8 +3,24 @@ package exemplo01;
 public class TesteCarro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Carro carro = new Carro();
+		carro.marca = "Ford";
+		carro.modelo = "KA";
+		carro.numPassageiros = 5;
+		carro.capCombustivel= 60;
+		carro.consumoCombustivel = 8;
+		
+		//simples sem retorno
+		carro.exibirCapacidade();
+		//com retorno 
+		double numeroTotalPassageiro = carro.obterNumPassageiro();
+		System.out.println(numeroTotalPassageiro);
+		//com paramentro
+		double qtdKm = carro.obterKm(10);
+		System.out.println(qtdKm);
+		
 	}
+	
 
 }
